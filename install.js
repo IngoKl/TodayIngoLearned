e7b4ace34d2c7fcadfe4514a8fe350433f62ecca
Ingo Kleiber <ikleiber@gmail.com>
2020-06-23 00:19:46 +0200

    Add copy mode to backupDb

--- install.js
+++ install.js
@@ -5,17 +5,31 @@ const fs = require('fs');
 var config = require('./config.json');
 
 // Create a backup of the current database and rename it based on the current datetime
-function backupDb() {
+function backupDb(mode='copy') {
     // Backup old database
     if (fs.existsSync(config.dbpath)) {
-        fs.renameSync(config.dbpath, `./db/til-${Date.now()}.db`);
+        if (mode == 'copy') {
+            fs.copyFile(config.dbpath, `./db/til-${Date.now()}.db`, (err) => { 
+                if (err) { 
+                  console.log("Error Found:", err); 
+                }
+            });
+        } else if (mode == 'rename') {
+            fs.renameSync(config.dbpath, `./db/til-${Date.now()}.db`);   
+        }
     }
 }
 
 // Create a new database
 function createDb() {
-    backupDb();
+    backupDb('rename');
     db.createdb.newDb();
+}
+
+// Refresh the tags in a database
+function refreshTags() {
+    backupDb('copy');
+    helpers.refreshTags();
 }
 
 // CLI
@@ -33,7 +47,7 @@ else if (command == 'setuserpassword') {
     helpers.changeUserPassword(process.argv.slice(2)[1], process.argv.slice(2)[2])
 }
 else if (command == 'refreshtags') {
-    helpers.refreshTags();
+    refreshTags();
 }
 else {
     console.log('install.js createdb|populatedb|adduser|setuserpassword|refreshtags');
